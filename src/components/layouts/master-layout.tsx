6d2b3ca7fd487a64ed101b4e39735b3408e570f1
Lucky Anggara <kerjaanlucky@gmail.com>
2025-11-17 08:01:19 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/layouts/master-layout.tsx
+++ src/components/layouts/master-layout.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { SidebarProvider, SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Search } from "lucide-react";
