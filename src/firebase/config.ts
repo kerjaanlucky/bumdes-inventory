507c207a00d329da59e383f816acc9ef38dd94bc
Lucky Anggara <kerjaanlucky@gmail.com>
2025-11-30 09:47:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9131393973-17d9b",
+  "appId": "1:654359184600:web:1daf92c776486c4f0903ad",
+  "apiKey": "AIzaSyCI1LOr8Y1OHWN_CG-_tLDhfTnOZrcXNSQ",
+  "authDomain": "studio-9131393973-17d9b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "654359184600"
+};
